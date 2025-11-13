c56579a4c58e6a5ca3562c2d99aa7fa6f0146185
sant32 <44911121+njmond@users.noreply.github.com>
2025-11-14 06:06:33 +0700

    Implement TEAFactory contract for pair creation

---
+++ contracts/TEAFactory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "./TEAPair.sol";
+
+contract TEAFactory {
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'PAIR_EXISTS');
+        
+        bytes memory bytecode = type(TEAPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        TEAPair(pair).constructor(token0, token1);
+        
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair;
+        allPairs.push(pair);
+        
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+}
